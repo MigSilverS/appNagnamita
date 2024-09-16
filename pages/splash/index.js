6cc796b408d5f012986bea0d5e7061facf383c6f
Miguel Prata Silva <48215025+MigSilverS@users.noreply.github.com>
2024-09-16 11:05:38 -0300

    Video Splash

--- pages/splash/index.js
+++ pages/splash/index.js
@@ -3,10 +3,10 @@ import { Video, ResizeMode, onPlaybackStatusUpdate } from "expo-av"
 
 export function Splash() {
     return (
-        <Video 
-        style={ StyleSheet.absoluteFill}
+        <Video
+        style={ StyleSheet.absoluteFill }
         resizeMode={ResizeMode.CONTAIN} 
-        source={require('../../assets/img/Nagnamita.gif')}
+        source={require('../../assets/img/Nagnamita.mp4')}
         isLooping={true}
         onPlaybackStatusUpdate={onPlaybackStatusUpdate}
         shouldPlay={true}
